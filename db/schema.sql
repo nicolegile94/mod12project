f138bf5e6909580b3bc6f0908ab773373561e29b
Nicole Gile <nicolegile94@gmail.com>
2020-11-06 21:48:03 -0600

    initial table creation

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,18 @@ CREATE TABLE departments (
     id INTEGER PRIMARY KEY,
     name VARCHAR(30)
 );
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,12 @@ VALUES
     ('Marketing'),
     ('Accounting');
 
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Manager', 65000, 2),
+    ('Secretary', 40000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ('John', 'Doe', 2),
+    ('Frank', 'Smith', 1);
