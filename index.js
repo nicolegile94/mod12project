472555d45a07500d0c49427650d0338b9765dd53
Nicole Gile <nicolegile94@gmail.com>
2020-11-07 21:17:09 -0600

    wrap up

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ function mainMenu() {
                 {
                     name: "Update an Employee",
                     value: "putEmployee"
+                },
+                {
+                    name: "Quit",
+                    value: "quit"
                 }
             ]
         }
@@ -66,6 +70,9 @@ function mainMenu() {
             case "putEmployee":
                 putEmployee();
                 break;
+           case "quit":
+                quit();
+                break;
             default:
                 quit();
         }
@@ -73,31 +80,128 @@ function mainMenu() {
 }
 
 function getDepartment() {
-    
-    console.table([
-        
-      ]);
+    // code to get list of departments
+
+    //then put the rows into console table
+    var values = [
+        ['Name of Department A'],
+        ['Name of Department B']
+    ]
+    console.table(['Department'], values);
+
+    mainMenu();
 }
 
 function getRole() { 
+    // code to get list of roles
+
+    //then put the rows into console table
+    var values = [
+        ['Name of Role A', 'Salary A'],
+        ['Name of Role B', 'Salary B']
+    ]
+    console.table(['Role', 'Salary'], values);
+
+    mainMenu();
+
 }
 
 function getEmployee() {
+    // code to get list of employees
+
+    //then put the rows into console table
+    var values = [
+        ['First Name of Employee A', 'Last Name of Employee A', 'Role ID A', 'Manager ID A' ],
+        ['First Name of Employee B', 'Last Name of Employee B', 'Role ID B', 'Manager ID B']
+    ]
+    console.table(['First Name', 'Last Name', 'Role', 'Manager'], values);
+
+    mainMenu();
 }
 
 function postDepartment() {
-}
+    prompt([
+        {
+            name: "name",
+            message: "What would you like to name this Department?"
+        }
+    ])
+    .then(res => {
+        //code to plug info into api
+
+        //return to main menu
+        mainMenu();
+    })
+};
 
 function postRole() {
+    prompt([
+        {
+            name: "name",
+            message: "What would you like to name this Role?"
+        },
+        {
+            name: "salary",
+            message: "What is this new Role's salary?"
+        }
+    ])
+    .then(res => {
+        //code to plug info into api
+
+        //return to main menu
+        mainMenu();
+    })
 }
 
 function postEmployee() {
+    prompt([
+        {
+            name: "firstName",
+            message: "What is this Employee's first name?"
+        },
+        {
+            name: "lastName",
+            message: "What is this Employee's last name?"
+        },
+        {
+            name: "roleID",
+            message: "What is this Employee's role?"
+        },
+        {
+            name: "managerID",
+            message: "Who is this Employee's manager?"
+        }
+    ])
+    .then(res => {
+        //code to plug info into api
+
+        //return to main menu
+        mainMenu();
+    })
 }
 
 function putEmployee() {
+    prompt([
+        {
+            name: "name",
+            message: "What is the name of the Employee you would like to edit??"
+        },
+        {
+            name: "newRole",
+            message: "What is the Employee's new role?"
+        }
+    ])
+    .then(res => {
+        //code to plug info into api
+
+        //return to main menu
+        mainMenu();
+    })
 }
 
 function quit() {
+    console.log("Thank you, goodbye!");
+    process.exit();
 }
 
 mainMenu();
